d546e51df789680beb27b052fa24a71f6e4c2c11
John Dunn <johnwdunn20@gmail.com>
2024-03-05 23:48:10 -0700

    outlined plan

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import requests
+import bs4
+
+# selections
+month = 3
+day = 6
+year = 2024
+
+# urls
+search_url = f"https://www.spotery.com/search?psLangId=EN&psAddrCity=San%20Francisco&psReservationDateStr={month}/{day}/{year}&psTimeRangeId=MORNING&psSourceFlow=SPOT&psOrgaSk=2654382&psIsGridView=false&psFavorites=false"
+
+# search urls require that you click a button to get availability so may be more challenging to scrape. I can either send a request for each spot (in the UI, you click on an a tag) or load all your favorite spots to see
+
+# Basic ideas:
+'''
+Need to be able to login (if you login, you might be able to save favorite places)
+Should be able to look for a date (or dates) and a time range
+Need to limit to near you (could pre-configure the close urls or the names of places)
+If it's available, should book
+It should only book one
+'''
